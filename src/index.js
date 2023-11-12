f58f0c5b7ae424b3fba53333309746d0a6ed4137
George Sevastiadis <gsevasti@netcompany-intrasoft.com>
2023-11-13 00:09:44 +0200

    creating js functions inside footer component

--- src/index.js
+++ src/index.js
@@ -73,6 +73,12 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
   return (
     <footer>{new Date().toLocaleTimeString()} . We are currently open!</footer>
   );
